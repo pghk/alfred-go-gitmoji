60de8c1b54f0b86bb42b4e333205b8c9d09d9789
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-04-24 01:37:58 -0400

    ✨ Download gitmoji list from github

--- internal/get_gitmoji.go
+++ internal/get_gitmoji.go
@@ -1,1 +1,31 @@
 package internal
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const GithubUrl string = "https://raw.githubusercontent.com"
+const GitmojiIndex string = "carloscuesta/gitmoji/master/src/data/gitmojis.json"
+
+func getGitmojiList(client *http.Client) string {
+	resp, err := client.Get(GithubUrl + "/" + GitmojiIndex)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body)
+}
--- internal/get_gitmoji_test.go
+++ internal/get_gitmoji_test.go
@@ -1,1 +1,33 @@
 package internal
+
+import (
+	"github.com/dnaeon/go-vcr/v2/recorder"
+	"github.com/kinbiko/jsonassert"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func setup(name string) (*http.Client, *recorder.Recorder) {
+	vcr, err := recorder.New("../test/_fixtures/" + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &http.Client{Transport: vcr}, vcr
+}
+
+func teardown(vcr *recorder.Recorder) {
+	err := vcr.Stop()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Test_getGitmojiList(t *testing.T) {
+	client, vcr := setup("get_gitmoji")
+	ja := jsonassert.New(t)
+
+	ja.Assertf(getGitmojiList(client), `{"gitmojis": "<<PRESENCE>>"}`)
+
+	teardown(vcr)
+}
